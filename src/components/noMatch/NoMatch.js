ab6a95e82f859035e3878ee82f6f4eb9a3193bc5
Michael Vitollo <ace51689@gmail.com>
2020-08-10 14:28:55 -0400

    Added dynamic URL for the welcome component, and a 404 page when no matching route is found.

---
+++ src/components/noMatch/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoMatch (props) {
+    return (
+        <div className="NoMatch">
+            404: Page Not Found
+        </div>
+    )
+}
+
+export default NoMatch
---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome(props) {
+    return (
+        <div className="Welcome">
+            <strong>Welcome, {props.name}!</strong>
+        </div>
+    )
+
+}
+
+export default Welcome
